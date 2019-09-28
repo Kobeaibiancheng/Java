e6eefb21d674daaa25e633e6cf3a1a3f074170bc
kobeaibiancheng <wyx160413@yeah.net>
2019-09-28 21:25:55 +0800

    Java第一次提交

---
+++ Test.java
@@ -0,0 +1,33 @@
+
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		/*char ch1 = '9';
+		char ch2 = 'w';
+		char ch3 = 300;
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		System.out.println("hello java!");
+		System.out.println("hello world!");
+		int a = 10;
+		int b = 20;
+		String str1 = "hello";
+		String str2 = "java";
+		System.out.println(str1+str2);
+		System.out.println("a = "+a);
+		System.out.println(b + a);
+		
+		String str = "result = ";
+		int a = 10;
+		int b = 20;
+		String result = str + a + b;
+		(result)*/
+		
+		int a = 10;
+		int b = 20;
+		System.out.println("a = " + a +",b = " + b);
+	}
+}
