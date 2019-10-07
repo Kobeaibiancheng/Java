aeb0bc819cab9681ba33b19357531b9be88837bf
kobeaibiancheng <wyx160413@yeah.net>
2019-10-07 20:47:16 +0800

    Java练习

---
+++ 20191007/Test.java
@@ -0,0 +1,14 @@
+
+
+public class Test{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 20;
+		int tmp = a;
+		a = b;
+		b = tmp;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
