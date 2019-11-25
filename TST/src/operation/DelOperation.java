4639b5c9ee73b0d67c90273e5aee4161062381d8
kobeaibiancheng <wyx160413@yeah.net>
2019-11-25 22:21:34 +0800

    继承接口多态实例

--- TST/src/operation/DelOperation.java
+++ TST/src/operation/DelOperation.java
@@ -1,11 +1,35 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 public class DelOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
         System.out.println("删除书籍");
+        System.out.println("请输入要删除的书籍的书名");
+        String name = scan.next();
+        Book book = null;
+        int i = 0;
+        for (i = 0; i < bookList.getUsedSize(); i++) {
+            book = bookList.getBook(i);
+            if(book.getName().equals(name)) {
+                break;
+            }
+        }
+        if(i == bookList.getUsedSize()) {
+            System.out.println("没有要删除的书籍");
+        }
+        else {
+            //删除书籍
+            for (int j = i; j < bookList.getUsedSize()-1; j++) {
+                book = bookList.getBook(j+1);
+                bookList.setBooks(j,book);
+            }
+            bookList.setUsedSize(bookList.getUsedSize()-1);
+            System.out.println("删除成功");
+        }
+
     }
 
 
--- TST/src/operation/FindOperation.java
+++ TST/src/operation/FindOperation.java
@@ -1,10 +1,22 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 public class FindOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
         System.out.println("查找书籍");
+        System.out.println("请输入要查找的书籍");
+        String name = scan.next();
+        Book book = null;
+        for (int i = 0; i < bookList.getUsedSize(); i++) {
+            book = bookList.getBook(i);
+            if(book.getName().equals(name)) {
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("没有要查找的书籍");
     }
 }
