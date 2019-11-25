e2975b72fd2005bbf76fccdf34d2eab0bad1c8a3
kobeaibiancheng <wyx160413@yeah.net>
2019-11-25 21:56:50 +0800

    类和对象

--- 20191028/src/Person.java
+++ 20191028/src/Person.java
@@ -2,7 +2,42 @@ public class Person {
     //成员属性
     private String name;
     private int age;
+    private String sex;
+    {
+        this.name = "小明";
+        this.age = 19;
+        this.sex = "man";
+        System.out.println("hi");
+    }
     //成员方法
+    public void show(){
+        System.out.println("name: " + name + " age: " + age + " sex: " + sex);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
     public void study(int num) {
             switch(num) {
                 case 1:
@@ -16,16 +51,22 @@ public class Person {
                     break;
                 case 4:
                     System.out.println("Physics");
+
                     break;
                 default:
                     System.out.println("Don't have this course");
                     break;
             }
     }
-
+/*    public void eat() {
+        System.out.println("吃饭");
+    }
+    public void study() {
+        System.out.println("学习");
+    }
     //构造方法
     public Person() {
-        System.out.println();
+        System.out.println("hello");
     }
     public Person(String name) {
         this.name = name;
@@ -37,5 +78,14 @@ public class Person {
         System.out.println("姓名:" + this.name);
         System.out.println("age:" + this.age);
     }
+    public Person(String name,int age,String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        System.out.println("姓名:" + this.name);
+        System.out.println("age:" + this.age);
+        System.out.println("sez:" + this.sex);
+    }*/
+
 
 }
--- 20191028/src/Test.java
+++ 20191028/src/Test.java
@@ -1,13 +1,36 @@
+class Sta {
+    public static int a = 0;
+    public int count = 0;
+    static {
+        a = 10;
+        System.out.println("a = " + a);
+    }
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
 public class Test {
     public static void main(String[] args) {
-        Calculator cal1 = new Calculator();
+
+/*        Sta sta1 = new Sta();
+        Sta sta2 = new Sta();*/
+
+/*      Calculator cal1 = new Calculator();
         Person per1 = new Person();
         Person per2 = new Person("zhangsan");
         Person per3 = new Person("lisi",18);
         per1.study(5);
-/*      System.out.println(cal1.add(2,3));
+        System.out.println(cal1.add(2,3));
         System.out.println(cal1.div(2,3));
         System.out.println(cal1.mul(2,3));
         System.out.println(cal1.sub(2,3));*/
+        Person person = new Person();
+        person.setName("李四");
+        person.setSex("man");
+        person.setAge(19);
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+        System.out.println(person.getSex());
+
     }
 }
