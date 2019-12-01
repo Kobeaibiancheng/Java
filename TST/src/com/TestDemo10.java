9e43d81dc71c84b025d865e6d2f8b0e5d08c2ffe
kobeaibiancheng <wyx160413@yeah.net>
2019-12-01 22:43:00 +0800

    字符串练习

---
+++ TST/src/com/TestDemo10.java
@@ -0,0 +1,25 @@
+package com;
+
+public class TestDemo10 {
+    public static int balancedStringSplit(String str) {
+        int count = 0;
+        int tmp = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'L') {
+                tmp++;
+            }
+            else {
+                tmp--;
+            }
+            if (tmp == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String str = "RLLLLRRRLR";
+        int ret = balancedStringSplit(str);
+        System.out.println(ret);
+    }
+}
