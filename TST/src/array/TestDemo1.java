656f94952152f283d43947131b1fe89e26cc31be
kobeaibiancheng <wyx160413@yeah.net>
2020-02-02 20:31:31 +0800

    plus one

--- TST/src/array/TestDemo1.java
+++ TST/src/array/TestDemo1.java
@@ -3,7 +3,20 @@ package array;
 
 public class TestDemo1 {
 
+    public static int[] plusOne(int[] digits) {
 
+        for (int i = digits.length-1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i]%10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[digits.length+1];
+        digits[0] = 1;
+        return digits;
+
+    }
     /**
      * 寻找数组的中心索引
      * @param nums
