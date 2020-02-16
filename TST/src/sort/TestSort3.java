62c8af3aed34a405a6fb284ee62d4a8647443662
kobeaibiancheng <wyx160413@yeah.net>
2020-02-16 22:21:55 +0800

    归并排序

--- TST/src/sort/TestSort3.java
+++ TST/src/sort/TestSort3.java
@@ -99,10 +99,126 @@ public class TestSort3 {
             shell(arr,drr[i]);
         }
     }
+
+
+    /**
+     * 归并排序:  分治思想
+     *时间复杂度：O(n*log2n)
+     *空间复杂度：O(n)
+     * 稳定性：稳定的
+     *
+     * 递归写法
+     * @param arr
+     */
+    /*public static void mergeSort(int[] arr) {
+        mergeSortInternal(arr,0,arr.length-1);
+    }
+
+    private static void mergeSortInternal(int[] arr,int left,int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSortInternal(arr,left,mid);
+        mergeSortInternal(arr,mid+1,right);
+        //合并
+        merge(arr,left,mid,right);
+
+    }
+
+    private static void merge(int[] arr,int left,int mid,int right) {
+        int start1 = left;
+        int start2 = mid+1;
+        *//**
+         *   3   2   1   9   7   5   6
+         *  left       mid         right
+         *  s1              s2
+         *//*
+        int[] tmpArr = new int[right-left+1];
+        int i = 0;//tmpArr的索引
+
+        while (start1 <= mid && start2 <= right) {
+            if (arr[start1] <= arr[start2]) {
+                tmpArr[i++] = arr[start1++];
+            }else {
+                tmpArr[i++] = arr[start2++];
+            }
+        }
+
+        //循环退出后，有两种情况，一种是 start1 指向的还有剩余元素，另一种是 start2
+        //指向的还有剩余元素
+        while (start1 <= mid) {
+            tmpArr[i++] = arr[start1++];
+        }
+        while (start2 <= right) {
+            tmpArr[i++] = arr[start2++];
+        }
+
+        //tmpArr里存放是的有序的数据，现在需要将tmpArr里的数据放回到arr中
+        for (int j = 0; j < tmpArr.length; j++) {
+            arr[left + j] = tmpArr[j];
+        }
+    }*/
+
+
+    /**
+     * 归并排序
+     * 非递归
+     * @param arr
+     */
+    public static void mergeSort(int[] arr) {
+        for (int i = 1; i < arr.length; i *= 2) {
+            merge(arr,i);
+        }
+    }
+    //gap代表每个归并段的数据
+    public static void merge(int[] arr,int gap) {
+
+        int[] tmpArr = new int[arr.length];
+        int k = 0;//下标
+
+        int s1 = 0;
+        int e1 = s1 + gap - 1;
+        int s2 = e1 + 1;
+        int e2 = (s2 + gap - 1) < arr.length ? s2 + gap - 1 : arr.length-1;
+
+        while (s2 < arr.length) {
+            while (s1 <= e1 && s2 <= e2) {
+                if (arr[s1] <= arr[s2]) {
+                    tmpArr[k++] = arr[s1++];
+                }else {
+                    tmpArr[k++] = arr[s2++];
+                }
+            }
+
+            while (s1 <= e1) {
+                tmpArr[k++] = arr[s1++];
+            }
+
+            while (s2 <= e2) {
+                tmpArr[k++] = arr[s2++];
+
+            }
+
+            s1 = e2 + 1;
+            e1 = s1 + gap -1;
+            s2 = e1 + 1;
+            e2 = (s2 + gap - 1) < arr.length ? s2 + gap - 1 : arr.length-1;
+
+        }
+        //判断是不是还有一个归并段，且这个归并段是 s1 那一段
+        while (s1 <= arr.length-1) {
+            tmpArr[k++] = arr[s1++];
+        }
+        for (int i = 0; i < tmpArr.length; i++) {
+            arr[i] = tmpArr[i];
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {4,3,6,8,2,1,9,5,10,7};
         System.out.println(Arrays.toString(arr));
-        shellSort(arr);
+        mergeSort(arr);
         System.out.println("===========================================");
         System.out.println(Arrays.toString(arr));
     }
