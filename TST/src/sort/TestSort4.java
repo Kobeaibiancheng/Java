a891cb744a8017d98614f89604881b129fabb9aa
kobeaibiancheng <wyx160413@yeah.net>
2020-02-16 22:22:56 +0800

    Arrays.sort()自定义类型

--- TST/src/sort/TestSort4.java
+++ TST/src/sort/TestSort4.java
@@ -1,8 +1,9 @@
 package sort;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-class Student implements Comparable{
+class Student implements Comparable<Student>{
     protected String name;
     protected int score;
     protected int age;
@@ -23,16 +24,40 @@ class Student implements Comparable{
     }
 
     @Override
-    public int compareTo(Object o) {
-        Student student = (Student)o;
+    public int compareTo(Student o) {
+
+        return this.age - o.age;
         //年龄按从小到大
+        /*
+        Student student = (Student)o;
         if (this.age > student.age) {
             return 1;
         }else if (this.age < student.age) {
             return -1;
         }else {
             return 0;
+        }*/
+    }
+}
+class ComparatorByName implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1 == o2) {
+            return 0;
         }
+        if (o1 == null) {
+            return -1;
+        }
+        if (o2 == null){
+            return 1;
+        }
+        return o1.name.compareTo(o2.name);
+    }
+}
+class ComparatorByAge implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.age - o2.age;
     }
 }
 public class TestSort4 {
@@ -46,7 +71,9 @@ public class TestSort4 {
         Student student2 = new Student("李四",70,18);
         Student student3 = new Student("王五",90,30);
         Student[] students = {student1,student2,student3};*/
-        Arrays.sort(students);
+        ComparatorByAge comparatorByAge = new ComparatorByAge();
+        ComparatorByName comparatorByName = new ComparatorByName();
+        Arrays.sort(students,comparatorByName);
         System.out.println(Arrays.toString(students));
     }
 }
