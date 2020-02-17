943d8a670fad196f7e246d44983ebc3caec5d9ea
kobeaibiancheng <wyx160413@yeah.net>
2020-02-17 20:32:44 +0800

    集合框架

--- TST/src/collectionsframework/TestDemo1.java
+++ TST/src/collectionsframework/TestDemo1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class TestDemo1 {
     public static void main(String[] args) {
         //Collection接口
-        Collection<String> collection = new ArrayList<>();
+        /*Collection<String> collection = new ArrayList<>();
         collection.add("java");
         collection.add("hello");
         System.out.println(collection.isEmpty());
@@ -14,16 +14,55 @@ public class TestDemo1 {
         collection.add("2019");
         System.out.println(collection);
         Object[] strings = collection.toArray();
-        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(strings));*/
+
+        Collection<String> list = new ArrayList<>();
+        list.add("I");
+        list.add(" miss");
+        list.add(" you");
+        list.add(" kobe");
+        list.add(" !");
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
+        System.out.println(list.remove(" !"));
+        for (String s : list) {
+            System.out.print(s);
+        }
+        System.out.println();
+        Object[] myWords = list.toArray();
+        System.out.println(Arrays.toString(myWords));
+        System.out.println(list);
+        list.clear();
+        System.out.println(list);
+        /*false
+        5
+        true
+        I miss you kobe
+        [I,  miss,  you,  kobe]*/
+
         /*for (String s : collection) {
             System.out.println(s);
         }*/
+
+        Map<String,String> map = new HashMap<>();
+        map.put("Kobe","basketball");
+        map.put("Gigi","daught");
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.get("Kobe"));
+        map.getOrDefault("霍华德","扣篮");
+        System.out.println(map.containsKey("Kobe"));
+        System.out.println(map.containsValue("basketball"));
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
 
         /**
          * map接口
          * <>指定数据类型是什么
          */
-        Map<String,String> map = new HashMap<>();
+        /*Map<String,String> map = new HashMap<>();
 
         map.put("国民女神","高圆圆");
         map.put("国民老公","王思聪");
@@ -36,7 +75,7 @@ public class TestDemo1 {
         System.out.println(map.containsKey("国民老公"));
         System.out.println(map.containsValue("李逵"));
         System.out.println(map.size());
-        System.out.println(map.isEmpty());
+        System.out.println(map.isEmpty());*/
 
     }
 }
