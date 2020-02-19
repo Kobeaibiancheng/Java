7fd207163be681d2a5cfef1544606c62e36bc873
kobeaibiancheng <wyx160413@yeah.net>
2020-02-19 21:39:20 +0800

    栈的实现

--- TST/src/collectionsframework/MyStack.java
+++ TST/src/collectionsframework/MyStack.java
@@ -46,3 +46,39 @@ public class MyStack<T> {
         return this.elem[this.top-1];
     }
 }
+
+class MyListStack<T>{
+    //定义一个内部类 就可以直接使用类型参数
+    class Node{
+        T data;
+        Node next;
+    }
+
+    private Node first;
+    private int size;
+    //构造方法
+    public MyListStack() {
+    }
+    //入栈
+    public void push(T data) {
+        Node oldfirst = first;
+        first = new Node();
+        first.data = data;
+        first.next = oldfirst;
+        size++;
+    }
+    //出栈
+    public boolean pop() {
+        T data = first.data;
+        first = first.next;
+        size--;
+        return true;
+    }
+    //获得栈顶元素
+    public T peek(){
+        return first.data;
+    }
+    public int size() {
+        return size;
+    }
+}
