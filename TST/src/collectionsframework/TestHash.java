2ce8d38a5c8651151959991cc7a301496be13b0b
kobeaibiancheng <wyx160413@yeah.net>
2020-02-21 22:30:12 +0800

    测试用例

---
+++ TST/src/collectionsframework/TestHash.java
@@ -0,0 +1,16 @@
+package collectionsframework;
+
+public class TestHash {
+    public static void main(String[] args) {
+        HashBucket hashBucket = new HashBucket();
+        hashBucket.put(1,20);
+        hashBucket.put(7,30);
+        hashBucket.put(9,44);
+        hashBucket.put(3,12);
+        hashBucket.put(4,15);
+        //hashBucket.put(0,42);
+        //hashBucket.put(5,66);
+        System.out.println("adc");
+    }
+
+}
