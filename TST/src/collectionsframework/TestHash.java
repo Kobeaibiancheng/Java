fed98792249ad66eb2e07445234ccc4a5ee0cc4f
kobeaibiancheng <wyx160413@yeah.net>
2020-02-23 21:13:37 +0800

    Map

--- TST/src/collectionsframework/TestHash.java
+++ TST/src/collectionsframework/TestHash.java
@@ -1,8 +1,10 @@
 package collectionsframework;
 
+import java.util.Scanner;
+
 public class TestHash {
     public static void main(String[] args) {
-        HashBucket hashBucket = new HashBucket();
+        /*HashBucket hashBucket = new HashBucket();
         hashBucket.put(1,20);
         hashBucket.put(7,30);
         hashBucket.put(9,44);
@@ -10,7 +12,10 @@ public class TestHash {
         hashBucket.put(4,15);
         //hashBucket.put(0,42);
         //hashBucket.put(5,66);
-        System.out.println("adc");
+        System.out.println("adc");*/
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
     }
 
 }
