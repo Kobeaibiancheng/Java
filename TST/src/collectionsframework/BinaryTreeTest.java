b8575d95ccdfb76db4547238b78c6ebd832a1aef
kobeaibiancheng <wyx160413@yeah.net>
2020-02-27 21:13:03 +0800

    测试用例

--- TST/src/collectionsframework/BinaryTreeTest.java
+++ TST/src/collectionsframework/BinaryTreeTest.java
@@ -3,18 +3,19 @@ package collectionsframework;
 public class BinaryTreeTest {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
-        TreeNode root = binaryTree.createTree();
+        TreeNode root = binaryTree.createTree("ABC##DE#G##F###");
+        //TreeNode root = binaryTree.createTree();
 
-        binaryTree.postOrderTraversal(root);
+        /*binaryTree.postOrderTraversal(root);
         System.out.println();
         System.out.println(binaryTree.postOrderTraversalNor(root));
-        /*binaryTree.inOrderTraversal(root);
+        binaryTree.inOrderTraversal(root);
         System.out.println();
         System.out.println(binaryTree.inOrderTraversalNor(root));*/
         //binaryTree.preOrderTraversal(root);
         //System.out.println(binaryTree.preOrderTraversalNor(root));
 
-        System.out.println(binaryTree.levelOrder(root));
+        binaryTree.preOrderTraversal(root);
     }
 
 }
