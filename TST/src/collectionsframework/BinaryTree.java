06dab121ad6e74b7af50cafc161834772eaee8ad
kobeaibiancheng <wyx160413@yeah.net>
2020-02-29 22:10:48 +0800

    二叉树

--- TST/src/collectionsframework/BinaryTree.java
+++ TST/src/collectionsframework/BinaryTree.java
@@ -368,6 +368,28 @@ public class BinaryTree {
         return root;
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode leftTree = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightTree = lowestCommonAncestor(root.right,p,q);
+
+        if (leftTree != null && rightTree != null) {
+            return root;
+        }
+        if (leftTree != null) {
+            return leftTree;
+        }
+        if (rightTree != null) {
+            return rightTree;
+        }
+        return null;
+    }
     
 
 }
