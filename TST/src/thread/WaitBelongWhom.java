3e3f402a48fef6b5b1aa44f1c55840aa07f4c7fe
kobeaibiancheng <wyx160413@yeah.net>
2020-03-27 22:49:04 +0800

    wait() notify() notifyAll()

---
+++ TST/src/thread/WaitBelongWhom.java
@@ -0,0 +1,10 @@
+package thread;
+
+public class WaitBelongWhom {
+    public static void main(String[] args) throws InterruptedException {
+        Object o = new Object();
+        o.wait();
+        o.notify();
+        o.notifyAll();
+    }
+}
