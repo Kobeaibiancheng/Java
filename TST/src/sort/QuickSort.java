ff9f8e7ca15da294a7ea2c9e682b64c96d8e218a
kobeaibiancheng <wyx160413@yeah.net>
2020-03-31 22:42:34 +0800

    快速排序

--- TST/src/sort/QuickSort.java
+++ TST/src/sort/QuickSort.java
@@ -2,6 +2,7 @@ package sort;
 
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class QuickSort {
     public static void quickSort(int[] arr,int left,int right) {
@@ -11,7 +12,8 @@ public class QuickSort {
             quickSort(arr,pivot+1,right);
         }
     }
-    private static int partion(int[] arr,int left,int right) {
+    // Hoare 法:
+    /*private static int partion(int[] arr,int left,int right) {
         int i = left;
         int j = right;
         int pivot = arr[left];
@@ -29,22 +31,78 @@ public class QuickSort {
 
             //i j 未相遇    -》 交换 下标i和j的值
             if (i < j) {
-                /*int tmp = arr[i];
+                int tmp = arr[i];
                 arr[i] = arr[j];
-                arr[j] = tmp;*/
+                arr[j] = tmp;
                 swap(arr,i,j);
             }
         }
         //i j 相遇   -》  交换arr[i] 和基准 pivot(arr[left])
         swap(arr,i,left);
         return i;
-    }
+    }*/
 
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
+
+
+    //挖坑法 ：
+    /*private static int partion(int[] arr,int left,int right) {
+        int i = left;
+        int j = right;
+        int pivot = arr[left];
+        while (i < j) {
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            arr[i] = arr[j];
+            while (i < j && arr[i] <= pivot) {
+                i++;
+            }
+            arr[j] = arr[i];
+        }
+        arr[i] = pivot;
+        return i;
+    }*/
+
+    //前后遍历法：
+    private static int partion(int[] arr, int left, int right) {
+        int d = left + 1;
+        int pivot = arr[left];
+        for (int i = left + 1; i <= right; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, d);
+                d++;
+            }
+        }
+        swap(arr, d - 1, left);
+        return d - 1;
+    }
+
+
+
+    //非递归分治
+    public static void quickSort(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr.length - 1);
+        stack.push(0);
+        while (!stack.isEmpty()) {
+            int left = stack.pop();
+            int right = stack.pop();
+            if (left >= right) {
+                continue;
+            }
+            int pivotIndex = partion(arr, left, right);
+            stack.push(right);
+            stack.push(pivotIndex + 1);
+            stack.push(pivotIndex - 1);
+            stack.push(left);
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {6,1,2,7,9,3,4,5,10,8};
