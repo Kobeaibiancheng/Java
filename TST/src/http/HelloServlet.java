c7e93a717f118edf1ac760c1611ab286231c6fec
kobeaibiancheng <wyx160413@yeah.net>
2020-04-05 22:33:39 +0800

    servlet

---
+++ TST/src/http/HelloServlet.java
@@ -0,0 +1,27 @@
+package http;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    //GET 请求时的处理机制
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().println("<h1>GET</h1>");
+        resp.getWriter().println("<p>你好世界</p>");
+    }
+
+
+    //POST 请求时的处理机制
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().println("<h1>POST</h1>");
+        resp.getWriter().println("<p>你好世界</p>");
+    }
+}
