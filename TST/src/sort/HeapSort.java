1438c29e7181bb6baf30a699f63c67b739a3210a
kobeaibiancheng <wyx160413@yeah.net>
2020-04-07 16:51:23 +0800

    堆排

--- TST/src/sort/HeapSort.java
+++ TST/src/sort/HeapSort.java
@@ -4,21 +4,31 @@ import java.util.Arrays;
 
 public class HeapSort {
     public static void main(String[] args) {
-        int[] arr = {2,3,1,6,4,8,9,7,10,5};
+        int[] arr = {1,5,3,8,7,6};
         heapSort(arr);
         System.out.println(Arrays.toString(arr));
     }
-
-    public static void createHeap(int[] arr) {
-        for (int i = (arr.length-1-1)/2; i >= 0; i--) {
-            adjustDown(arr,i,arr.length);
+    public static void heapSort(int[] arr) {
+        createHeap(arr);
+        int end = arr.length-1;
+        while (end > 0) {
+            int tmp = arr[0];
+            arr[0] = arr[end];
+            arr[end] = tmp;
+            end--;
+            adjustDown(arr,0,end);
         }
     }
-    public static void adjustDown(int[] arr,int i,int len) {
+    public static void createHeap(int[] arr) {
+        for (int i = (arr.length-1-1)/2; i >= 0; i--) {
+            adjustDown(arr,i,arr.length-1);
+        }
+    }
+    public static void adjustDown(int[] arr,int i,int end) {
         int parent = i;
         int child = parent*2+1;
-        while(child < len) {
-            if (child+1 < len && arr[child] < arr[child+1]) {
+        while(child <= end) {
+            if (child+1 <= end && arr[child] < arr[child+1]) {
                 child = child+1;
             }
             if (arr[child] > arr[parent]) {
@@ -30,19 +40,8 @@ public class HeapSort {
             }else {
                 break;
             }
-
         }
     }
-    public static void heapSort(int[] arr) {
-        createHeap(arr);
-        int end = arr.length-1;
-        while (end > 0) {
-            int tmp = arr[0];
-            arr[0] = arr[end];
-            arr[end] = tmp;
 
-            adjustDown(arr,0,end);
-            end--;
-        }
-    }
 }
+
