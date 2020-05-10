fbb034c722a128083068171ea4951acaae8aad11
kobeaibiancheng <wyx160413@yeah.net>
2020-05-10 22:23:17 +0800

    实现strStr

--- TST/src/string/TestDemo1.java
+++ TST/src/string/TestDemo1.java
@@ -955,10 +955,35 @@ public class TestDemo1 {
     }
 
 
+    /**
+     * 实现strStr
+     * 给定一个 haystack 字符串和一个 needle 字符串，
+     * 在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public static int strStr(String haystack, String needle) {
 
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        if(len2 == 0 || needle == null){
+            return 0;
+        }
+        if(len1==0|| len1<len2) {
+            return -1;
+        }
+        for(int i = 0; i <= len1-len2; i++) {
+            if(haystack.substring(i,i+len2).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-
-
+        String str1 = "a";
+        String str2 = "";
+        System.out.println(strStr(str1, str2));
         /*Scanner sc = new Scanner(System.in);
         while(sc.hasNext()) {
             String str = sc.nextLine();
