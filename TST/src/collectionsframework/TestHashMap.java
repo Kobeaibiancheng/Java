6cd2af1cadaa7c254f1784d8547bd33e1ed1c53d
kobeaibiancheng <wyx160413@yeah.net>
2020-05-13 21:42:24 +0800

    HashMap

---
+++ TST/src/collectionsframework/TestHashMap.java
@@ -0,0 +1,11 @@
+package collectionsframework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+
+    }
+}
