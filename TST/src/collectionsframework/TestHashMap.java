2c0ba51d71dca263429ea998b7530588b3a720b7
kobeaibiancheng <wyx160413@yeah.net>
2020-05-15 22:12:34 +0800

    HashMap

--- TST/src/collectionsframework/TestHashMap.java
+++ TST/src/collectionsframework/TestHashMap.java
@@ -2,10 +2,19 @@ package collectionsframework;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TestHashMap {
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<>();
-
+        map.put("2","l");
+        map.put("16","miss");
+        map.put("0","you");
+        map.put("100","kobe");
+        Set<Map.Entry<String,String>> set = map.entrySet();
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + " ");
+            System.out.println(entry.getValue());
+        }
     }
 }
