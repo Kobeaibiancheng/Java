60b366d5e2ef03738fbba84093c9ff77bfbd5df1
kobeaibiancheng <wyx160413@yeah.net>
2020-10-26 22:09:22 +0800

    排序自定义类型

--- study/src/programming/Student.java
+++ study/src/programming/Student.java
@@ -1,5 +1,8 @@
 package programming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Student implements Comparable<Student>{
     String name;
     double score;
@@ -21,4 +24,26 @@ public class Student implements Comparable<Student>{
                 ", score=" + score +
                 '}';
     }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int n = Integer.valueOf(sc.nextLine());
+            Student[] students = new Student[n];
+            for (int i = 0; i < n; i++) {
+                String str = sc.nextLine();
+                String[] strings = str.split(" ");
+                students[i] = new Student(strings[0],Double.parseDouble(strings[1]));
+            }
+
+            ComparatorByScore comparatorByScore = new ComparatorByScore();
+            ComparatorByName comparatorByName = new ComparatorByName();
+
+            Arrays.sort(students,comparatorByScore);
+            //Arrays.sort(students);
+            System.out.println(Arrays.toString(students));
+        }
+    }
+
 }
