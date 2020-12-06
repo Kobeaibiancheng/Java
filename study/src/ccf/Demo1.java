9c00cf681dcb50bec41bdb3484d04adbc3b7b81f
kobeaibiancheng <wyx160413@yeah.net>
2020-12-06 23:01:53 +0800

    ccf疫情检测点

--- study/src/ccf/Demo1.java
+++ study/src/ccf/Demo1.java
@@ -1,33 +1,49 @@
 package ccf;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-
-class point{
+class Point implements Comparable<Point>{
     int x;
     int y;
-    int 
+    double distance;
+    int sign;
+
+    @Override
+    public int compareTo(Point o) {
+        return (int)this.distance - (int)o.distance;
+    }
+
+    public int getSign() {
+        return sign;
+    }
 }
 public class Demo1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<Integer,Double> map = new HashMap<>();
         int n = sc.nextInt();
         int X = sc.nextInt();
         int Y = sc.nextInt();
-        double[] arr = new double[n];
+        Point[] arr = new Point[n];
+        /*int[] qu = new int[3];
+        for (int i = 0; i < qu.length; i++) {
+            qu[i] = 1000001;
+        }*/
         for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            arr[i] = (X-x)*(X-x) + (Y-y)*(Y-y);
-            map.put(i,arr[i]);
+            Point point = new Point();
+            point.x = sc.nextInt();
+            point.y = sc.nextInt();
+            point.distance = (Math.pow(X-point.x,2) + Math.pow(Y-point.y,2));
+            point.sign = i+1;
+            arr[i] = point;
+            /*double tmp = point.distance;
+            if (tmp < qu[0]) {
+                if ()
+            }*/
         }
         Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            System.out.print( + " ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arr[i].getSign());
         }
     }
 }
