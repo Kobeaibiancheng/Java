bee47aa19a6e7c128a047e667dd6a55979432205
kobeaibiancheng <wyx160413@yeah.net>
2021-01-28 18:21:44 +0800

    ccf回收站选址

--- study/src/programming/Recycling.java
+++ study/src/programming/Recycling.java
@@ -10,42 +10,34 @@ class L {
 public class Recycling {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.valueOf(sc.nextLine());
-        List<L> list = new ArrayList<>();
-
+        int n = sc.nextInt();
         int[] num = new int[5];
-        int k = 0;
         L[] node = new L[n];
-
-
         for (int i = 0; i < n; i++) {
-            String[] strings = sc.nextLine().split(" ");
-            int x = Integer.valueOf(strings[0]);
-            int y = Integer.valueOf(strings[1]);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
             L l = new L();
             l.x = x;
             l.y = y;
             node[i] = l;
         }
         for (int i = 0; i < n; i++) {
+            //标志是否符合回收站 == 4 表示符合
             int count = 0;
+            //回收站得分数
             int cut = 0;
             for (int j = 0; j < n; j++) {
                 if (node[i].x + 1 == node[j].x && node[i].y == node[j].y) {
                     count++;
-                    //cut++;
                 }
                 if (node[i].x - 1 == node[j].x && node[i].y == node[j].y) {
                     count++;
-                    //cut++;
                 }
                 if (node[i].y + 1 == node[j].y && node[i].x == node[j].x) {
                     count++;
-                    //cut++;
                 }
                 if (node[i].y - 1 == node[j].y && node[i].x == node[j].x) {
                     count++;
-                    //cut++;
                 }
                 if (node[i].x + 1 == node[j].x && node[i].y + 1 == node[j].y) {
                     cut++;
@@ -61,10 +53,24 @@ public class Recycling {
                 }
             }
             if (count == 4) {
-                num[k] = cut;
-                k++;
+                switch (cut) {
+                    case 0:
+                        num[0] = 0;
+                        break;
+                    case 1:
+                        num[1]++;
+                        break;
+                    case 2:
+                        num[2]++;
+                        break;
+                    case 3:
+                        num[3]++;
+                        break;
+                    case 4:
+                        num[4]++;
+                        break;
+                }
             }
-
         }
         for (int i = 0; i < num.length; i++) {
             System.out.println(num[i]);
