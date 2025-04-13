4e7c700b70cbdc1238d2d369569feb12f9ad8de7
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-04-13 14:52:46 +0800

    重载的学习

---
+++ TST/src/chongzai/Demo2.java
@@ -0,0 +1,19 @@
+package chongzai;
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int print(int x,int y){
+        int sum = x+y;
+        System.out.println("两个整数相加等于："+ sum);
+        return x+y;
+    }
+    public static double print(int x,int y){
+        int sum = x+y;
+        System.out.println("两个整数相加等于："+ sum);
+        return x+y;
+    }
+}
